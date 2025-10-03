cc322f17dd69c27db3fc82e8467c3c84f16c4a1c
Rajesh-RL <rajesh@rocketlane.com>
2025-10-04 01:59:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("experienceBtn")?.addEventListener("click", () => {
+  window.location.href = "experience.html";
+});
+
+document.getElementById("backBtn")?.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
